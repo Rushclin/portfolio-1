c634b71886d03e6d9f437e1b3c1c4f74c2944703
rushclin02 <takamrushclin@gmail.com>
2023-01-14 03:31:09 +0100

    Navbar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@
 /*************************************/
 
 "use-strict";
+
+const header = document.querySelector("header");
+
+/* STICKY NAVBAR */
+
+const stickyNavbar = () => {
+  header.classList.toggle("scrolled", window.pageYOffset > 0);
+};
+
+stickyNavbar();
+
+window.addEventListener("scroll", stickyNavbar);
