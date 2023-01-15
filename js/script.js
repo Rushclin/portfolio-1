745ea3554c25b64277665d94d816eb012d5321c3
rushclin02 <takamrushclin@gmail.com>
2023-01-15 19:17:18 +0100

    Finish showcase info and showcase image

--- js/script.js
+++ js/script.js
@@ -17,3 +17,11 @@ const stickyNavbar = () => {
 stickyNavbar();
 
 window.addEventListener("scroll", stickyNavbar);
+
+let sr = ScrollReveal({
+  duration: 2500,
+  distance: "60px",
+});
+
+sr.reveal(".showcase-info", { delay: 600 });
+sr.reveal(".showcase-image", { origin: "top", delay: 700 });
