fc8866a95d71950cc3864ee89c18a256563836bb
rushclin02 <takamrushclin@gmail.com>
2023-01-20 00:03:37 +0100

    Finish show-case info

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,12 @@ const stickyScroll = () => {
 stickyScroll();
 
 window.addEventListener("scroll", stickyScroll);
+
+/* SCROLL REVEAL */
+let sr = ScrollReveal({
+  duration: 2500,
+  distance: "60px",
+});
+
+sr.reveal(".showcase-info", { delay: 600 });
+sr.reveal(".showcase-image", { origin: "top", delay: 700 });
