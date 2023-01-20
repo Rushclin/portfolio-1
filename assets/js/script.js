fbd051951824b56b2898040758b755fb2fdf7ff0
rushclin02 <takamrushclin@gmail.com>
2023-01-20 22:29:02 +0100

    Add active section on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@
 const header = document.querySelector("header");
 const logoSpans = document.querySelectorAll(".logo-span");
 
+const links = document.querySelectorAll(".nav-link");
+
 const stickyScroll = () => {
   header.classList.toggle("scrolled", window.pageYOffset > 0);
   logoSpans.forEach((logoSpan) => {
@@ -18,7 +20,10 @@ const stickyScroll = () => {
 
 stickyScroll();
 
-window.addEventListener("scroll", stickyScroll);
+window.addEventListener("scroll", () => {
+  stickyScroll();
+  activeLink();
+});
 
 /* SCROLL REVEAL */
 let sr = ScrollReveal({
@@ -28,3 +33,28 @@ let sr = ScrollReveal({
 
 sr.reveal(".showcase-info", { delay: 600 });
 sr.reveal(".showcase-image", { origin: "top", delay: 700 });
+
+/* Change Active on scroll */
+
+const activeLink = () => {
+  let sections = document.querySelectorAll("section[id]");
+
+  let passedSection = Array.from(sections)
+    .map((section, id) => {
+      return {
+        y: section.getBoundingClientRect().top - header.offsetHeight,
+        id: id,
+      };
+    })
+    .filter((sct) => sct.y <= 0);
+
+  let curentSectionID = passedSection.at(-1).id;
+
+  links.forEach((link) => {
+    link.classList.remove("active");
+  });
+
+  links[curentSectionID].classList.add("active");
+};
+
+activeLink();
