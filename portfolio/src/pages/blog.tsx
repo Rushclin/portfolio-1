0cd8dccadc46abffec740c326ed502e10c760818
rushclin02 <takamrushclin@gmail.com>
2023-08-05 19:58:10 +0200

    Blog Page

---
+++ portfolio/src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const Blog = () => {
+  return (
+    <Layout pageTitle="Blog">
+      <h1>Blog</h1>
+    </Layout>
+  );
+};
+
+export default Blog;
