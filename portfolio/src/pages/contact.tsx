865e2d80b18ac7c79136c3ee15b1a471d8f81428
rushclin02 <takamrushclin@gmail.com>
2023-08-05 19:56:26 +0200

    Add Contact file

---
+++ portfolio/src/pages/contact.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const Contact = () => {
+  return (
+    <Layout pageTitle="Contact">
+      <h1>Contact</h1>
+    </Layout>
+  );
+};
+
+export default Contact;
