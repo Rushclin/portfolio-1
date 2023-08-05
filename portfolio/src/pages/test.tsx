13f2c6908134ebfd504d7d7d7acd6e728e4a7730
rushclin02 <takamrushclin@gmail.com>
2023-08-05 19:56:37 +0200

    Test file

--- portfolio/src/pages/test.tsx
+++ portfolio/src/pages/test.tsx
@@ -1,10 +1,17 @@
 import Cookies from "@/components/Cookies";
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Head from "next/head";
 
 const Test = () => {
   return (
     <>
-      {/* <Hero /> */}
+      <Head>
+        <title>RUSHCLIN TAKAM • Front-End and Mobile Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Hero />
+      <Header />
       <Cookies />
     </>
   );
