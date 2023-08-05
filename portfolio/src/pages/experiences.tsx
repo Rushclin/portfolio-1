1cf13de05576ae92c51d1b053ad8a87bb396df95
rushclin02 <takamrushclin@gmail.com>
2023-08-05 19:58:03 +0200

    Experience page

---
+++ portfolio/src/pages/experiences.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const Experiences = () => {
+  return (
+    <Layout pageTitle="Experiences">
+      <h1>Experiences</h1>
+    </Layout>
+  );
+};
+
+export default Experiences;
