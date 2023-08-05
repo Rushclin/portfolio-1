d58bfced6817f7d19b503619ddbf2e33c0fde57c
rushclin02 <takamrushclin@gmail.com>
2023-08-05 19:58:18 +0200

    About Page

---
+++ portfolio/src/pages/about.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <h1>Hello about page</h1>
+    </Layout>
+  );
+};
+
+export default AboutPage;
