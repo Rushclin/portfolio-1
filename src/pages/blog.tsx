bda42c74db60cd9f12f641adc66cff65a0fec917
rushclin02 <takamrushclin@gmail.com>
2023-08-06 16:15:22 +0200

    Comming soon on Blog Page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,10 +1,21 @@
 import Layout from "@/components/Layout";
 import React from "react";
+import localFont from "next/font/local";
+
+const abrilFactface = localFont({
+  src: "./../../public/fonts/AbrilFatface-Regular.woff",
+});
 
 const Blog = () => {
   return (
     <Layout pageTitle="Blog">
-      <h1>Blog</h1>
+      <div className="min-h-[calc(100vh-8rem)] w-full flex justify-center items-center content-center">
+        <h1
+          className={`md:text-7xl sm:text-5xl text-3xl heading ${abrilFactface.className}`}
+        >
+          Comming soon
+        </h1>
+      </div>
     </Layout>
   );
 };
